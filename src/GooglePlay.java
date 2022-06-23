8829407fa42c4f6b84b99d0278569a77c754110d
ika sukmawati <ikasukmawati005@gmail.com>
2022-06-23 17:22:33 +0700

    Initial commit

---
+++ src/GooglePlay.java
@@ -0,0 +1,15 @@
+public class GooglePlay {
+
+    String name;
+    int speed;
+    int healthPoin;
+
+    void run(){
+        System.out.println(name +" is running...");
+        System.out.println("Speed: "+ speed);
+    }
+
+    boolean isDead(){
+        if(healthPoin <= 0) return true;
+        return false;
+
---
+++ src/teori/txt.java
@@ -0,0 +1,12 @@
+package teori;
+
+public class txt {
+    //soal(1) : masuk intelj bikin kelas baru,menggunakan 'int'tipedata bilangan bulat(tipe data numerik)
+    //mengetik nodeleft dan right sebagai data itu akan dikanan kiri,key=item,setelah itu left = null,
+    //data diproses pada bagaian bawah lalu return
+
+    //soal(2):mengurutkan data dengan memeecahkan kumpulan data,nilai pivot yang dipilih ditempatkan,pada posisi partisi
+    //(int start, int end), steelah selesai menempatkan pivot,proses dilanjut secara rekursif,
+
+    //soal(3)JSONAPI
+}
